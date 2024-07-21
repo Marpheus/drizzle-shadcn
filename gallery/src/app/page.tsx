a743b66ddce3b877c9187e64bdfbd75717f2e4b5
Maros Vasilisin <maros.vasilisin@futured.app>
2024-07-21 14:54:15 +0200

    move to grid

--- gallery/src/app/page.tsx
+++ gallery/src/app/page.tsx
@@ -8,8 +8,21 @@ export const dynamic = "force-dynamic";
 async function Images() {
   const images = await getMyImages();
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image) => (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {[
+        ...images,
+        ...images,
+        ...images,
+        ...images,
+        ...images,
+        ...images,
+        ...images,
+        ...images,
+        ...images,
+        ...images,
+        ...images,
+        ...images,
+      ].map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
           <Link href={`/img/${image.id}`}>
             {/*// this cost money https://vercel.com/docs/image-optimization/limits-and-pricing*/}
