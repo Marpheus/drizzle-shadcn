9b57480407c5c5713eca9f5fa369ae4903761003
Maros Vasilisin <maros.vasilisin@futured.app>
2024-07-21 13:52:36 +0200

    next image

--- gallery/src/app/page.tsx
+++ gallery/src/app/page.tsx
@@ -1,15 +1,23 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getMyImages } from "~/server/queries";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
   const images = await getMyImages();
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} alt="image" className="h-auto w-full" />
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          {/*// this cost money https://vercel.com/docs/image-optimization/limits-and-pricing*/}
+          <Image
+            src={image.url}
+            alt={image.name}
+            style={{ objectFit: "contain" }}
+            width={192}
+            height={192}
+          />
           <div>{image.name}</div>
         </div>
       ))}
