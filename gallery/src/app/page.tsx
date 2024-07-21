ec6ec100401845ba7ecf385e0d92a64c01ccdc5e
Maros Vasilisin <maros.vasilisin@futured.app>
2024-07-21 10:41:42 +0200

    db setup

--- gallery/src/app/page.tsx
+++ gallery/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/0d62c1a7-015a-41c1-952f-767141bcad1b-urw2mp.jpg",
   "https://utfs.io/f/ad2b4b25-3a6b-4077-a628-cbf30b788d45-d9lcld.jpg",
@@ -10,13 +12,21 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} alt="image" className="h-auto w-full" />
+          </div>
+        ))}
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
           </div>
         ))}
       </div>
