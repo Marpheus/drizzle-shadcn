963c04f50e319953bd7ac1675464404dc2067449
Maros Vasilisin <maros.vasilisin@futured.app>
2024-07-21 16:50:09 +0200

    removed repeat

--- gallery/src/app/page.tsx
+++ gallery/src/app/page.tsx
@@ -9,20 +9,7 @@ async function Images() {
   const images = await getMyImages();
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      {[
-        ...images,
-        ...images,
-        ...images,
-        ...images,
-        ...images,
-        ...images,
-        ...images,
-        ...images,
-        ...images,
-        ...images,
-        ...images,
-        ...images,
-      ].map((image) => (
+      {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
           <Link href={`/img/${image.id}`}>
             {/*// this cost money https://vercel.com/docs/image-optimization/limits-and-pricing*/}
